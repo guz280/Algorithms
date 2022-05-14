dcf54fba07e9f51b180ee560e9c612295369b156
Joseph Cauchi <jcauchi@novibet.com>
2022-05-14 23:46:54 +0200

    update roman numbers

--- ConsoleApp2/RomanNumbers.cs
+++ ConsoleApp2/RomanNumbers.cs
@@ -8,40 +8,10 @@ namespace ConsoleApp2
 {
 	public static class RomanNumbers
 	{
-        public static int RomanToInt_JC(string s)
-        {
-            Dictionary<char, int> romanNumbersDictionary = new()
-            {
-                { 'I', 1 },
-                { 'V', 5 },
-                { 'X', 10 },
-                { 'L', 50 },
-                { 'C', 100 },
-                { 'D', 500 },
-                { 'M', 1000 }
-            };
-
-
-
-            int sum = 0;
-            for (int i = 0; i < s.Length; i++)
-            {
-                //char currentRomanChar = s[i];
-                romanNumbersDictionary.TryGetValue(s[i], out int num);
-                if (i + 1 < s.Length && romanNumbersDictionary[s[i + 1]] > romanNumbersDictionary[s[i]])
-                {
-                    sum -= num;
-                }
-                else
-                {
-                    sum += num;
-                }
-            }
-            return sum;
-        }
-
-
         // https://leetcode.com/problems/roman-to-integer/
+        //I can be placed before V (5) and X (10) to make 4 and 9. 
+        // X can be placed before L(50) and C(100) to make 40 and 90. 
+        // C can be placed before D(500) and M(1000) to make 400 and 900.
         public static int RomanToInt(string s)
         {
             int sum = 0;
@@ -75,5 +45,40 @@ namespace ConsoleApp2
 
             return sum;
         }
+
+        public static int RomanToInt_JC(string s)
+        {
+            Dictionary<char, int> romanNumbersDictionary = new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+
+
+            int sum = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                //char currentRomanChar = s[i];
+                romanNumbersDictionary.TryGetValue(s[i], out int num);
+                if (i + 1 < s.Length && romanNumbersDictionary[s[i + 1]] > romanNumbersDictionary[s[i]])
+                {
+                    sum -= num;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+
+
+        
     }
 }
